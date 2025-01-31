dfc981703bc2d772b4d28f9c2a5d677e009d1990
Vbarbosa <131600563+yogurlapiz@users.noreply.github.com>
2025-01-31 20:56:53 +0000

    First Commit

---
+++ app.js
@@ -0,0 +1,27 @@
+( 6/* eslint-disable */
+    import "bootstrap";
+    import "./style.css";
+    
+    import "./assets/img/rigo-baby.jpg";
+    import "./assets/img/4geeks.ico";
+    
+    window.onload = function() {
+      let getWho = ["The dog", "My grandma", "The mailman", "My bird"];
+      let getAction = ["ate", "peed", "crushed", "broke"];
+      let getWhat = ["my homework", "my phone", "the car", "my book"];
+      let getWhen = [
+        "before the class",
+        "when I was sleeping",
+        "while I was exercising",
+        "during my lunch",
+        "while I was praying"
+      ];
+    
+      function getExcuse() {
+        return getWho.concat(
+          Math.floor(Math.random(getAction, getWhat, getWhen, "."));
+    
+      }
+    
+      document.getElementById("excuse").innerHTML = getExcuse();
+    };
